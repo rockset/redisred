8695d2ae79fbd1bd46e7560c0acc42540e52c9d0
Vahid Fazel-Rezai <vahid@rockset.com>
2018-07-30 21:43:52 +0000

    support prefix-defined alias paths

--- routes/main.js
+++ routes/main.js
@@ -17,17 +17,34 @@ module.exports = function (redis) {
   })
 
   // redirect or show 404
-  router.get('/:redirect_name*', function(req, res) {
-    var redirectName = req.params.redirect_name;
-    if (req.params['0']) redirectName += req.params['0'];
-    Redirect.get(redirectName, function(err, redirect) {
-      if (err)
-        res.status(500).send(err);
-      else if (!redirect)
-        res.status(404).render('404');
-      else
-        res.redirect(redirect.url);
-    });
+  router.get('/:redirect_alias*', function(req, res) {
+
+    // get full query path
+    var queryPath = req.params.redirect_alias;
+    if (req.params['0']) queryPath += req.params['0'];
+
+    var aliasParts = queryPath.split('/');
+
+    // find longest prefix that is defined and append remaining parts to the path
+    // e.g. go/phacility/D123 will work if only go/phacility is defined
+    var tryRedirect = function (aliasParts, numAppendedParts) {
+        if (numAppendedParts >= aliasParts.length) {
+            res.status(404).render('404');
+        } else {
+            var key = aliasParts.slice(0, aliasParts.length - numAppendedParts).join('/');
+            var appendedPath = aliasParts.slice(aliasParts.length - numAppendedParts, aliasParts.length).join('/');
+            Redirect.get(key, function(err, redirect) {
+              if (err)
+                res.status(500).send(err);
+              else if (!redirect)
+                tryRedirect(aliasParts, numAppendedParts + 1);
+              else
+                res.redirect(redirect.url + appendedPath);
+            });
+        }
+    }
+
+    tryRedirect(aliasParts, 0);
   });
 
   // create or update redirect
